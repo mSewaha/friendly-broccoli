af0dbdd73df003279a4198ad9199b1e9380e0e84
Mina Sewaha <mina.sewaha@student.manchester.ac.uk>
2017-09-11 01:47:22 +0300

    updating data automatically on server instead of updating it when the user loads the site and updating success.html footer

--- myapp/views.py
+++ myapp/views.py
@@ -4,28 +4,31 @@ import json
 import os.path
 import datetime
 import socket
+import time
+import threading
 
 
 def getData():
 	scriptpath = os.path.dirname(__file__)
-	filename = os.path.join(scriptpath, '****')
+	filename = os.path.join(scriptpath, 'Unknownn')
 
 	config = {
-	  "apiKey": "****",
-	  "authDomain": "****",
-	  "databaseURL": "****",
-	  "storageBucket": "****",
+	  "apiKey": "AIzaSyA19-RiG_V7X-ugMLPbVZG-a02tTDROPgQ",
+	  "authDomain": "friendly-broccoli.firebaseapp.com",
+	  "databaseURL": "https://friendly-broccoli.firebaseio.com",
+	  "storageBucket": "friendly-broccoli.appspot.com",
 	  "serviceAccount": filename
 	}
 
 	firebase = pyrebase.initialize_app(config)
 
 	auth = firebase.auth()
-	user = auth.sign_in_with_email_and_password("****", "****")
+	user = auth.sign_in_with_email_and_password("random6767@mina.com", "mySuperStrongPass1")
 
 	#print (user)
 
 	db = firebase.database()
+
 
 	#getting the latest record
 	tempID = 0
@@ -62,8 +65,9 @@ def getData():
 	return temp, press, light, motion, timestamp, status
 
 
+temp, press, light, motion, timestamp,status = getData()
+
 def index(request):
-	temp, press, light, motion, timestamp,status = getData()
 	host =  request.META['HTTP_HOST']
 	return render(request, 'index.html', {"temp": temp, "press" : press, "light" : light, "motion": motion, "timestamp" : timestamp, "status":status, 'h': host})
 
@@ -71,3 +75,23 @@ def success(request):
 	#the host ipaddress and port
 	s = request.META['HTTP_HOST']
 	return render(request, 'success.html' )
+
+#updates data every one minute
+def updateData():
+	while True:
+		st = time.time()
+		print("started ", time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) )
+		global temp 
+		global light
+		global motion
+		global timestamp
+		global status
+		temp, press, light, motion, timestamp,status = getData()
+		fn = time.time()
+		print("finished ", time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()))
+		print("time elapsed: ", fn-st)
+		time.sleep(60)
+
+t = threading.Thread(target=updateData, args=[])
+t.setDaemon(True)
+t.start()
